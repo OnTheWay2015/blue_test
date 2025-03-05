97cf0bf6684cce3bdf1ddeeb0812b4efbb4b1f2b
OnTheWay2015 <7130458@qq.com>
2025-03-05 20:00:58 +0800

    。

--- ts_vscode_test/src/main.ts
+++ ts_vscode_test/src/main.ts
@@ -1,5 +1,15 @@
-
+const zlib = require('zlib');
 
 
 const hello : string = "Hello World!"
 console.log(hello)
+
+const input = '这是一段要被压缩的文本。';
+zlib.gzip(input, (err:any, result:any) => {
+  if (err) {
+    console.error('压缩时出错:', err);
+  } else {
+    console.log('压缩后的数据:', result);
+  }
+});
+
