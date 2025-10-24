1257de98b6961a212dbe59555b22afab8cbd8855
OnTheWay2015 <7130458@qq.com>
2025-10-24 12:48:54 +0800

    。

---
+++ ___helloworlds/python/helloword.py
@@ -0,0 +1,79 @@
+#coding:utf-8
+
+"""
+9种基本数据类型：
+    数值类型(整数、浮点数、复数)；
+    字节类型(字符串、字节串)；
+    组合类型(集合、元组、列表、字典)
+
+"""
+
+"""
+python注释的三种方法：
+    1.井号注释单行代码： #
+    2.三个单引号或三个双引号注释语句块： ''' 或者" " "
+    3.井号加两个百分号画出语句块分界线： #%%
+"""
+
+
+"""
+Python 保留字符, 所有 Python 的关键字只包含小写字母。
+
+and	exec	not
+assert	finally	or
+break	for	pass
+class	from	print
+continue	global	raise
+def	if	return
+del	import	try
+elif	in	while
+else	is	with
+except	lambda	yield
+
+"""
+
+"""
+多个语句构成代码组
+缩进相同的一组语句构成一个代码块，我们称之代码组。
+
+像if、while、def和class这样的复合语句，首行以关键字开始，以冒号( : )结束，该行之后的一行或多行代码构成代码组。
+
+我们将首行及后面的代码组称为一个子句(clause)。
+
+如下实例：
+
+if expression :
+   suite
+elif expression : 
+   suite  
+else :  
+   suite
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
