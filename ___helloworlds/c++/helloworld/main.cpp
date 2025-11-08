f2dc72f0b13a138fad2f6bb62196b4dfd816abe8
OnTheWay2015 <7130458@qq.com>
2025-11-09 00:46:36 +0800

    。

--- ___helloworlds/c++/helloworld/main.cpp
+++ ___helloworlds/c++/helloworld/main.cpp
@@ -3,9 +3,54 @@
 
 extern void test_set();
 
+
+void testAddress() 
+{
+    struct xst {
+    public:
+        int str[10];
+    };
+
+    xst ary[3];
+   
+
+    for (int i = 0; i < 3;i++ ) {
+        memset((ary[i].str), 0, 10);
+        //ary[i].str[0] = 1;
+    }
+
+    for (int i = 0; i < 3;i++ ) {
+        printf("str int:%d\n",ary[i].str[0]);
+    }
+
+    auto x = *(xst*)ary;
+    auto xx = &ary;
+
+    auto xxx = &x;
+
+    auto xxxx = (xst*)ary + 1;
+    xxxx->str[0] = 666;
+
+
+
+    unsigned long long aryP1 = (unsigned long)(&ary[0]); //取数组第一个元素的地址, 转为地址数值
+    //aryP1 和  aryP  值是一样的
+    unsigned long long aryP = (unsigned long)ary; //数组名是一个指针，直接可以转为地址数值
+    int s_xst = sizeof(xst);
+    auto xxxxx = (xst*)( (unsigned long long)ary + sizeof(xst)*2); //偏移两个数组元素，在第三个修改
+    xxxxx->str[0] = 999;
+}
+
+
+
+
+
+
+
 int main()
 {
-    test_set();
+    testAddress();
+    //test_set();
     return 0;
 }
 
