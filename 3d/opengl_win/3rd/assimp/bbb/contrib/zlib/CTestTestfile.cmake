6d9f749495b169a9b45f0315d4196d7afdd37283
OnTheWay2015 <7130458@qq.com>
2025-12-27 17:41:23 +0800

    。

---
+++ 3d/opengl_win/3rd/assimp/bbb/contrib/zlib/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: D:/git_work/blue_test/3d/opengl_win/3rd/assimp/contrib/zlib
+# Build directory: D:/git_work/blue_test/3d/opengl_win/3rd/assimp/bbb/contrib/zlib
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
